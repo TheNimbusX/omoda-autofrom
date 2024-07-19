50abe9f30f66ad6e8bc0cbbc917bea54de584b93
Denis Grab <yt.denisgrab@gmail.com>
2024-07-19 10:26:02 +0300

    text mistakes

--- scripts/script.js
+++ scripts/script.js
@@ -247,6 +247,14 @@ function init() {
     });
 
     myMap.geoObjects.add(placemark);
+    myMap.controls.remove('geolocationControl'); // удаляем геолокацию
+    myMap.controls.remove('searchControl'); // удаляем поиск
+    myMap.controls.remove('trafficControl'); // удаляем контроль трафика
+    myMap.controls.remove('typeSelector'); // удаляем тип
+    myMap.controls.remove('rulerControl'); // удаляем контрол правил
+    myMap.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+    map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+    // map.controls.remove('zoomControl'); // удаляем контрол зуммирования
 }
 
 //////////////////////////////////////////// complectations menu
